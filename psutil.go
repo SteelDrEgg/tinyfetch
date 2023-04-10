75b4e6402b3b581991aee1e7c779de7d164ac86d
DrEgg <102451227+SteelDrEgg@users.noreply.github.com>
2023-04-10 02:07:57 -0400

    complete some clean up

--- psutil.go
+++ psutil.go
@@ -13,25 +13,10 @@ var printMaterial []*kits.Tab
 
 func memFmt() {
 	memStat, _ := mem.VirtualMemory()
-	//printTab("RAM:\t")
 	var total string = kits.ProperUnit(memStat.Total)
 	var used string = kits.ProperUnit(memStat.Used)
 	var swap string = kits.ProperUnit(memStat.SwapTotal)
 	var usage string = kits.Float2string(memStat.UsedPercent) + "%"
-	//fmt.Print(used, " / ", total, " (", usage, ")", "\n")
-	//printTab("Swap:\t")
-	//fmt.Print(swap, "\n")
-	/*
-		memOut := kits.Tab{
-			Title: "RAM",
-			Content: used+" / "+total+" ("+usage+")",
-		}
-		swapOut := kits.Tab{
-			Title: "Swap",
-			Content: swap,
-		}
-		printMaterial=append(printMaterial, &memOut, &swapOut)
-	*/
 	printMaterial = append(
 		printMaterial,
 		&kits.Tab{Title: "RAM", Content: used + " / " + total + " (" + usage + ")"},
@@ -47,8 +32,6 @@ func cpuFmt() {
 	if len(cpuStat) > 1 {
 		count = fmt.Sprintf("x%v", len(cpuStat))
 	}
-	//printTab("CPU:\t")
-	//fmt.Print(name, " ("+cores+")", " "+count, "\n")
 	printMaterial = append(
 		printMaterial,
 		&kits.Tab{Title: "CPU", Content: name + " (" + cores + ")" + " " + count},
@@ -56,21 +39,10 @@ func cpuFmt() {
 }
 
 func diskFmt() {
-	/*partitions, _ := disk.Partitions(true)
-	var partiStats []string
-	for _, data := range partitions {
-		temp, _ := disk.Usage(data.Mountpoint)
-		total := kits.ProperUnit(temp.Total)
-		used := kits.ProperUnit(temp.Used)
-		usage := kits.Float2string()(temp.UsedPercent)
-		partiStats = append(partiStats, data.Mountpoint+" "+used+" / "+total+" "+usage+"%")
-	}*/
 	diskStat, _ := disk.Usage("/")
 	total := kits.ProperUnit(diskStat.Total)
 	used := kits.ProperUnit(diskStat.Used)
 	usage := kits.Float2string(diskStat.UsedPercent)
-	//printTab("Disk:\t")
-	//fmt.Print(used, " / ", total, " ("+usage+"%)", "\n")
 	printMaterial = append(
 		printMaterial,
 		&kits.Tab{Title: "Disk", Content: used + " / " + total + " (" + usage + "%)"},
@@ -91,15 +63,11 @@ func hostFmt() {
 	kernel := hostStat.OS
 	kernalVer := hostStat.KernelVersion
 
-	//printTab("OS:\t")
-	//fmt.Print(os, " ", osVer, " ", arch, "\n")
 	printMaterial = append(
 		printMaterial,
 		&kits.Tab{Title: "OS", Content: os + " " + osVer + " " + arch},
 	)
 
-	//printTab("Kernel:\t")
-	//fmt.Print(kernel, " ", kernalVer, "\n")
 	printMaterial = append(
 		printMaterial,
 		&kits.Tab{Title: "Kernel", Content: kernel + " " + kernalVer},
@@ -112,15 +80,5 @@ func main() {
 	memFmt()
 	diskFmt()
 
-	//fmt.Print(kits.Logos("linux"))
-	//for _, dt := range kits.Logos("apple"){
-	//	fmt.Println(dt)
-	//}
-	//var a [] []string
-	//a=append(a, []string{"a","b"})
-	//a=append(a, []string{"a","2"}, []string{"1","b"})
-	//for _, val := range printMaterial {
-	//	fmt.Println(*val)
-	//}
 	kits.PrintMaterial(printMaterial)
 }
