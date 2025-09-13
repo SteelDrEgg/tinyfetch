934bc28bfecf03bab3883d5c4bc4c9bb7b8a463e
DrEgg <dev@dregg.me>
2025-09-13 00:50:48 -0400

    Better ProperUnit

--- kits/util/formating.go
+++ kits/util/formating.go
@@ -1,26 +1,30 @@
 package util
 
 import (
-	"fmt"
+	"strconv"
 )
 
-func ProperUnit(byteNum uint64) (formatted string) {
-	byteNum = byteNum
-	if byteNum >= 1099511627776 {
-		// If the size over 1TiB
-		formatted = Float2string(float64(byteNum)/float64(1024*1024*1024*1024), 1) + "TiB"
-	} else if byteNum >= 1073741824 {
-		// If the size over 1GiB
-		formatted = Float2string(float64(byteNum)/float64(1024*1024*1024), 1) + "GiB"
-	} else if byteNum >= 1048576 {
-		// If the size over 1MiB
-		formatted = Float2string(float64(byteNum)/float64(1024*1024), 1) + "MiB"
-	} else if byteNum >= 1024 {
-		// If the size over 1KiB
-		formatted = Float2string(float64(byteNum)/float64(1024), 1) + "KiB"
-	} else {
-		// If the size is less than 1KiB
-		formatted = fmt.Sprintf("%v", byteNum) + "Bytes"
+func properUnitHelper(bytes uint64, pow uint8, unit string) string {
+	quotient := bytes >> pow
+	temp := bytes & ((1 << pow) - 1)
+	temp = ((temp * 10) + ((1 << pow) >> 1)) >> pow
+	if temp == 10 {
+		temp = 0
+		quotient += 1
 	}
-	return
+	return strconv.FormatUint(quotient, 10) +
+		"." + strconv.FormatUint(temp, 10) + " " + unit
+}
+
+func ProperUnit(byteNum uint64) (formatted string) {
+	if byteNum >= 1<<40 { // TiB
+		return properUnitHelper(byteNum, 40, "TiB")
+	} else if byteNum >= 1<<30 { // GiB
+		return properUnitHelper(byteNum, 30, "GiB")
+	} else if byteNum >= 1<<20 { // MiB
+		return properUnitHelper(byteNum, 20, "MiB")
+	} else if byteNum >= 1<<10 { // KiB
+		return properUnitHelper(byteNum, 10, "KiB")
+	}
+	return strconv.FormatUint(byteNum, 10) + " B"
 }
